5328eeca34398a375646bb605b95293a3172f244
mohdahmad15 <ahmadsoft15@gmail.com>
2022-03-04 14:45:16 +0530

    added something

--- src/main/java/com/jpa/demo/SpringBootJpaDemoApplication.java
+++ src/main/java/com/jpa/demo/SpringBootJpaDemoApplication.java
@@ -22,7 +22,7 @@ public class SpringBootJpaDemoApplication {
 		user.setName("Aamir");
 		user.setCity("Basti");
 		user.setStatus("JAVA Developer");
-
+		//this program told by Aamir Bhai
 		User user1=userRepository.save(user);
 		System.out.println(user1);
 
